46f9a07911b9497738d796e0c34383390819f2ff
LeoIsidro <90939274+LeoIsidro@users.noreply.github.com>
2022-08-29 10:22:18 -0500

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
-        'postgresql://postgres:UTEC@localhost:5432/postgres'
+        'postgresql://postgres:Princesa4@localhost:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
